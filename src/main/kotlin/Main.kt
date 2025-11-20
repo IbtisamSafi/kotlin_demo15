7b86036dd72a5158c040307991bcbe7214ce5790
IbtisamSafi <safiibtisam891@gmail.com>
2025-11-20 15:42:42 +0200

    i am happy

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,67 +1,40 @@
 package org.bytebloom
 
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+
+fun extractValue(input: String, key: String) : String {
+    val lowercase=input.lowercase()
+    val spases = input.split(";")
+    var spase1 = ""
+    for (space in spases) {
+        spase1=space.trim()
+        for (key in spase1){
+            val input1=readLine()?:"".lowercase()
+             if (input1.equals(key)){ return input1 }
+            else { null } }
+ }
+    return spase1
+
+
+}
+
+
+
+
 fun main() {
-    //---------- problems 1( personalized Greeting ) ----------
-    println("---------- problems 1( personalized Greeting ) ----------")
-    println("please enter your name: ")
-    val UserName=readLine()
-    println("please entre your favorite color :  ")
-    val FavoriteColor=readLine()
-    println("Hello : $UserName! I see your favorite color is  : $FavoriteColor.")
 
-
-
-    //---------- problems 2( Age in a Decade ) ----------
-    println("---------- problems 2( Age in a Decade ) ----------")
-    println(" please enter your age : ")
-    var UserAge=readLine()?.toInt()
-    UserAge = UserAge?.plus(10)
-    println("In 10 years , you will be : $UserAge years old.")
-
-
-
-    //---------- problems 3(Access Control )---------
-    println("---------- problems 3(Access Control )---------")
-    println("please enter your age : ")
-    val UserAge1= readLine()?.toInt()
-    val TheCheck= UserAge1!! >=18
-    println("Access Grantd : $TheCheck")
-
-
-
-    //--------- problems 4(Even or odd )---------
-    println("--------- problems 4(Even or odd )---------")
-    println("please enter the number to be checked : ")
-    val TheNum= readLine()?.toInt()
-    if(TheNum?.rem(2) ==0)
-       println("the number $TheNum is even.")
-    else
-        println("the number $TheNum is odd.")
-
-
-
-    //--------- problems 5(String Reverser)---------
-    println("--------- problems 5(String Reverser)---------")
-    println("please enter the text to be reversed  : ")
-    val TheText=readLine()
-    val TextReverse= TheText?.reversed()
-    println("the text reversed : $TextReverse .")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      val config = """
+        userId: user-123; role: admin;
+        authToken: token-XYZ-789 ;
+        environment : production; expiry: 1672531199  """
+   // println("entre the vale")
+   // val input1=readLine()?:""
+   // extractValue(input1,config)
+    println(extractValue(config,"role"))
 
 
 
